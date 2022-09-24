e3b60d7a011785c7cdcae701cda97faa34911c1a
feki ghassen <96629137+Ghassenfeki@users.noreply.github.com>
2022-09-24 10:42:43 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tabs from './navigation/Tabs';
+import backgroundimage from './navigation/background';
+import { Text } from 'react-native';
+const App = ({}) => {
+  return (
+       <Tabs />
+    );
+  }
+  export default App;
+  
+
+
+
+
+
